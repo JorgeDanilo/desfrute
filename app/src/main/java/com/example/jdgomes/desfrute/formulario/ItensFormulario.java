48596bd8ada5be39edcb8bb2548e47ff69d118c3
Jorge Danilo <danilo91519188@gmail.com>
2018-03-11 15:19:21 -0300

    add fragments itens

---
+++ app/src/main/java/com/example/jdgomes/desfrute/formulario/ItensFormulario.java
@@ -0,0 +1,5 @@
+package com.example.jdgomes.desfrute.formulario;
+
+
+public class ItensFormulario {
+}
---
+++ app/src/main/java/com/example/jdgomes/desfrute/fragment/ItensFragment.java
@@ -0,0 +1,27 @@
+package com.example.jdgomes.desfrute.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.jdgomes.desfrute.R;
+
+
+public class ItensFragment extends BaseFragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_itens, container, false);
+    }
+
+
+}
